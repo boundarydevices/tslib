559f8d0677525d27e0066551a88db2003bd5cead
Troy Kisky <troy.kisky@boundarydevices.com>
2017-04-10 10:07:54 -0700

    ts_open: restore ts_open availability
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- src/ts_open.c
+++ src/ts_open.c
@@ -18,13 +18,15 @@
 #endif
 #include <sys/fcntl.h>
 #include <stdio.h>
+#include <linux/fb.h>
 #include <linux/input.h>
 
 #include "tslib-private.h"
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 extern struct tslib_module_info __ts_raw;
 
-struct tsdev *ts_open(const char *name, int nonblock, int xres, int yres)
+static struct tsdev *ts_open_xy(const char *name, int nonblock, int xres, int yres)
 {
 	struct tsdev *ts;
 	int flags = O_RDWR;
@@ -38,7 +40,6 @@ struct tsdev *ts_open(const char *name, int nonblock, int xres, int yres)
 		ts->xres = xres;
 		ts->yres = yres;
 
-		printf("%s:screen resolution = %dx%d\n", __func__, ts->xres, ts->yres);
 		ts->fd = open(name, flags);
 		/*
 		 * Try again in case file is simply not writable
@@ -52,6 +53,7 @@ struct tsdev *ts_open(const char *name, int nonblock, int xres, int yres)
 			goto free;
 	}
 
+	printf("%s:%s:screen resolution = %dx%d\n", __func__, name, ts->xres, ts->yres);
 	return ts;
 
 free:
@@ -61,7 +63,7 @@ free:
 
 static const char * const input_choices[] = {"/dev/input/ts",
 	"/dev/input/event0", "/dev/input/event1",
-	"/dev/input/event2", "/dev/touchscreen/ucb1x00", NULL
+	"/dev/input/event2", "/dev/touchscreen/ucb1x00",
 };
 
 #define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
@@ -95,43 +97,99 @@ static int is_touchscreen(struct tsdev *ts)
 	return 1;
 }
 
+struct tsdev *ts_try_dev(const char *p, int nonblock, int xres, int yres)
+{
+	struct tsdev *ts;
+
+	if (!p)
+		return NULL;
+
+	ts = ts_open_xy(p, nonblock, xres, yres);
+	if (!ts)
+		return ts;
+	if (is_touchscreen(ts))
+		return ts;
+	ts_close(ts);
+	return NULL;
+}
+
+struct tsdev *ts_try_open(int nonblock, int xres, int yres, int index, const char **pp)
+{
+	const char *p = NULL;
+
+	if (index == -1) {
+		p = getenv("TSLIB_TSDEVICE");
+	} else if (index < (int)ARRAY_SIZE(input_choices)) {
+		p = input_choices[index];
+	}
+	*pp = p;
+	return ts_try_dev(p, nonblock, xres, yres);
+}
+
 struct tsdev *ts_open_config(int nonblock, int xres, int yres)
 {
 	struct tsdev *ts;
-	const char * const *pp = input_choices;
 	const char *p;
+	int i;
 
-	p = getenv("TSLIB_TSDEVICE");
-	if (p) {
-		ts = ts_open(p, nonblock, xres, yres);
-		if (!ts) {
-			perror("ts_open");
-			return ts;
-		}
-		if (ts_config(ts)) {
+	for (i = -1; i < (int)ARRAY_SIZE(input_choices); i++) {
+		ts = ts_try_open(nonblock, xres, yres, i, &p);
+		if (ts) {
+			if (!ts_config(ts)) {
+				printf("opened:%s\n", p);
+				return ts;
+			}
 			perror("ts_config");
 			ts_close(ts);
-			return NULL;
 		}
-		printf("opened %s\n", p);
-		return ts;
+	}
+	return NULL;
+}
+
+static char *defaultfbdevice = "/dev/fb0";
+
+struct tsdev *ts_open(const char *name, int nonblock)
+{
+	struct fb_var_screeninfo var;
+	struct tsdev *ts;
+	char *fbdevice;
+	const char *p;
+	int fb;
+	int xres = 0;
+	int yres = 0;
+	int i;
+
+	fbdevice = getenv("TSLIB_FBDEVICE");
+	if (!fbdevice)
+		fbdevice = defaultfbdevice;
+
+	fb = open(fbdevice, O_RDWR);
+	if (fb == -1) {
+		perror("open fbdevice");
+		goto c1;
 	}
 
-	for (;;) {
-		p = *pp++;
-		if (!p) {
-			perror("ts_open");
-			return NULL;
-		}
-		ts = ts_open(p, nonblock, xres, yres);
-		if (!ts)
-			continue;
-		if (is_touchscreen(ts)) {
-			if (!ts_config(ts))
-				break;
-		}
-		ts_close(ts);
+	if (ioctl(fb, FBIOGET_VSCREENINFO, &var) < 0) {
+		perror("ioctl FBIOGET_VSCREENINFO");
+		goto c1;
 	}
-	printf("opened %s\n", p);
-	return ts;
+
+	xres = var.xres;
+	yres = var.yres;
+c1:
+	close(fb);
+
+	ts = ts_try_dev(name, nonblock, xres, yres);
+	if (ts) {
+		printf("opened:%s\n", p);
+		return ts;
+	}
+	for (i = -1; i < (int)ARRAY_SIZE(input_choices); i++) {
+		ts = ts_try_open(nonblock, xres, yres, i, &p);
+		if (ts) {
+			printf("opened:%s\n", p);
+			return ts;
+		}
+	}
+	return NULL;
 }
